a40a28128e78b269dfc378dd98908fab9d4ddd8a
CaioAdriano1199 <caioadriano1199@gmail.com>
2025-10-23 20:33:50 -0300

    fazendo a tela de pontuação

--- src/app/components/telapontuacao/telapontuacao.js
+++ src/app/components/telapontuacao/telapontuacao.js
@@ -5,6 +5,12 @@ import Input from "../input/input";
 
 export default function Telapontuacao() {
   return (
-    <div className="p-8"></div>
+    <div className="min-h-screen p-8 flex flex-col items-center">
+      <h1 className="text-4xl font-bold mb-8 mt-4">Tela de Pontuação</h1>
+      <div className="flex flex-col items-center justify-center flex-grow">
+      <Input label={"valor"} placeholder="0"></Input>
+      <Button variant="primary"> gerar pontos</Button>
+      </div>
+    </div>
   );
 }
